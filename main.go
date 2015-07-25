2c55da56309f0d13c0c233772dd76e4119b5530c
Jaime Pillora <dev@jpillora.com>
2015-07-26 00:30:46 +1000

    added dns server option

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"net/url"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/jpillora/backoff"
 	"github.com/jpillora/opts"
+	"github.com/miekg/dns"
 	"github.com/shirou/gopsutil/load"
 	"github.com/shirou/gopsutil/mem"
 )
@@ -22,6 +25,7 @@ type config struct {
 	Database string        `help:"InfluxDB database"`
 	Interval time.Duration `help:"Time between reports"`
 	Tags     string        `help:"InfluxDB tags in the form \"<tag>=<value>,<tag>=<value>\""`
+	DNS      string        `help:"DNS server (used to lookup URL)"`
 }
 
 func main() {
@@ -52,33 +56,96 @@ func main() {
 
 	//good to go
 	log.Printf("Using InfluxDB endpoint: %s", u)
+	log.Printf("Current time is %s UTC", time.Now().Format(time.RFC3339))
 
-	b := backoff.Backoff{Max: 2 * c.Interval}
-	entries := ""
-	for {
-		e := bytes.Buffer{}
+	lock := sync.Mutex{}
+	entries := []string{}
+
+	send := func() error {
+		body := strings.NewReader(strings.Join(entries, "\n"))
+		if c.DNS != "" {
+			h, p, _ := net.SplitHostPort(u.Host)
+			ips, err := lookup(h, c.DNS)
+			if err != nil {
+				return fmt.Errorf("Lookup failed: %s", err)
+			}
+			u.Host = ips[0] + ":" + p
+		}
+		resp, err := http.Post(u.String(), "application/x-www-form-urlencoded", body)
+		if err != nil {
+			return fmt.Errorf("HTTP POST failed: %s", err)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusNoContent {
+			msg, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return fmt.Errorf("Response download failed: %s", err)
+			}
+			return fmt.Errorf("Response: %d => %s", resp.StatusCode, msg)
+		}
+		log.Printf("Success")
+		//clear once recieved!
+		entries = nil
+		return nil
+	}
+
+	report := func() {
+		t := time.Now().UnixNano()
 		if l, err := load.LoadAvg(); err == nil {
-			fmt.Fprintf(&e, "\ncpu_load_short%s value=%f", tags, l.Load1)
-			fmt.Fprintf(&e, "\ncpu_load_medium%s value=%f", tags, l.Load5)
-			fmt.Fprintf(&e, "\ncpu_load_long%s value=%f", tags, l.Load15)
+			entries = append(entries, fmt.Sprintf("cpu_load_short%s value=%f %d", tags, l.Load1*100, t))
+			entries = append(entries, fmt.Sprintf("cpu_load_medium%s value=%f %d", tags, l.Load5*100, t))
+			entries = append(entries, fmt.Sprintf("cpu_load_long%s value=%f %d", tags, l.Load15*100, t))
 		}
 		if v, err := mem.VirtualMemory(); err == nil {
-			fmt.Fprintf(&e, "\nmem_usage%s value=%f", tags, v.UsedPercent)
+			entries = append(entries, fmt.Sprintf("mem_usage%s value=%f %d", tags, v.UsedPercent, t))
 		}
-		entries += e.String()
+	}
 
-		if len(entries) > 0 {
-			resp, err := http.Post(u.String(), "application/x-www-form-urlencoded", strings.NewReader(entries))
-			if err != nil || resp.StatusCode != http.StatusNoContent {
-				if err != nil {
-					log.Printf("HTTP POST failed: %s", err)
+	//send loop
+	go func() {
+		b := backoff.Backoff{}
+		for {
+			wait := time.Second
+			lock.Lock()
+			if len(entries) > 0 {
+				if err := send(); err == nil {
+					b.Reset()
+				} else {
+					log.Println(err)
+					wait = b.Duration()
 				}
-				time.Sleep(b.Duration()) //wait a little extra
-			} else {
-				entries = "" //success - clear body
-				b.Reset()
 			}
+			lock.Unlock()
+			time.Sleep(wait)
 		}
+	}()
+
+	//report loop
+	for {
+		lock.Lock()
+		report()
+		lock.Unlock()
 		time.Sleep(c.Interval)
 	}
+}
+
+var dnsClient = dns.Client{}
+
+func lookup(domain, server string) ([]string, error) {
+	msg := dns.Msg{}
+	msg.SetQuestion(domain+".", dns.TypeA)
+	r, _, err := dnsClient.Exchange(&msg, server+":53")
+	if err != nil {
+		return nil, err
+	}
+	l := len(r.Answer)
+	if l == 0 {
+		return nil, fmt.Errorf("No answers")
+	}
+	ips := make([]string)
+	for i, answer := range r.Answer {
+		ans := answer.(*dns.A)
+		ips[i] = ans.A.String()
+	}
+	return ips, nil
 }
